ad86fd226288afe3c75e07eafeb0b710ce100b4a
Adi Kapuri <adikap1904@gmail.com>
2025-11-01 19:05:55 +0200

    hints addition to schema

--- server/src/models/CodeBlock.js
+++ server/src/models/CodeBlock.js
@@ -3,9 +3,13 @@ import mongoose from "mongoose";
 const CodeBlockSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
-    slug: { type: String, required: true, unique: true }, 
+    slug: { type: String, required: true, unique: true },
     initialCode: { type: String, default: "" },
-    solution: { type: String, default: "" }, 
+    solution: { type: String, default: "" },
+    hints: {
+      type: [String],
+      default: [],
+    },
   },
   { timestamps: true }
 );
